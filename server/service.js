59930ff83d8964fbd52e17ec336cbb01eb34d787
Divyansh Gupta <divyansh.2392@gmail.com>
2021-06-16 21:09:26 +0530

    Adding ServiceRegistry for handling service creation,removal logic at common place and to keep track of services

--- server/service.js
+++ server/service.js
@@ -1,16 +1,51 @@
-const express = require('express');
-
+const express = require("express");
+const ServiceRegistry = require("./lib/ServiceRegistry");
 const service = express();
 
 module.exports = (config) => {
   const log = config.log();
+  const serviceRegistry = new ServiceRegistry(log);
   // Add a request logging middleware in development mode
-  if (service.get('env') === 'development') {
+  if (service.get("env") === "development") {
     service.use((req, res, next) => {
       log.debug(`${req.method}: ${req.url}`);
       return next();
     });
   }
+
+  /*
+     Services Routes
+  */
+
+  service.put(
+    "/register/:servicename/:serviceversion/:serviceport",
+    (req, resp, next) => {
+      const { servicename, serviceversion, serviceport } = req.params;
+      //IP Address some machines could have ip v6 version, wrapping of IPv6 address
+
+      const serviceip = req.connection.remoteAddress.includes("::")
+        ? `[${req.connection.remoteAddress}]`
+        : req.connection.remoteAddress;
+
+      const serviceKey = serviceRegistry.register(
+        servicename,
+        serviceversion,
+        serviceip,
+        serviceport
+      );
+
+      return resp.json({ result: serviceKey });
+    }
+  );
+  service.delete(
+    "/register/:servicename/:serviceversion/:serviceport",
+    (req, resp, next) => {
+      return next("Not Implemented");
+    }
+  );
+  service.get("/find/:servicename/:serviceversion", (req, resp, next) => {
+    return next("Not Implemented");
+  });
 
   // eslint-disable-next-line no-unused-vars
   service.use((error, req, res, next) => {
